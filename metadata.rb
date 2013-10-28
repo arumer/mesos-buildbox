65027001fa1f5ecbd145e541e8cfcdc44c8ec381
Bernardo Gomez Palacio <bernardo.gomezpalacio@gmail.com>
2013-10-28 09:58:33 -0700

    Updating contact email address

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name              "mesos-buildbox"
 maintainer        "Bernardo Gomez Palacio"
-maintainer_email  "bernardo.palacio@guavus.com"
+maintainer_email  "bernardo.gomezpalacio@gmail.com"
 license           "Apache 2.0"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.0.1"
@@ -8,7 +8,7 @@ version           "0.0.1"
 depends           "yum"
 depends           "java"
 
-recipe "mesos-buildbox", "Installs software stack to build the Mesos RPM."
+recipe "mesos-buildbox", "Nothing right now. Please use mesos-buildbox::packages"
 recipe "mesos-buildbox:packages", "Installs development packages."
 
 %w{ redhat centos scientific amazon }.each do |os|
